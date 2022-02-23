ac584041f2734942254a5512033e387b2d57ddd3
manuelpra20 <manuelpra@gmail.com>
2022-02-23 12:56:05 -0400

    proyect add to github

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+
+export const environment = {
+  production: true,
+    firebaseConfig : {
+    apiKey: "AIzaSyCgwFCEfXZrfp9Cqp75xMYtxUyClHpfhOs",
+    authDomain: "manuelprado.firebaseapp.com",
+    projectId: "manuelprado",
+    storageBucket: "manuelprado.appspot.com",
+    messagingSenderId: "859932463436",
+    appId: "1:859932463436:web:306e914f5e11bad3e5bfe9"
+  }
+};
+
